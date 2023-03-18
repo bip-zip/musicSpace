97c2361136da50093d8d0f83322123df37c3c26b
bip-zip <bipindhakal44@gmail.com>
2023-03-18 14:26:58 +0545

    fix templates

--- musicmaker/views.py
+++ musicmaker/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'musicmaker/index.html'
+
--- musicspace/settings.py
+++ musicspace/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
 STATIC_DIRS = os.path.join(BASE_DIR,'staticfiles')
 
 
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'musicspace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIRS],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
