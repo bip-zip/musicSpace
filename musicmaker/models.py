1580068dba232e657440cdf7386dfbdb771a5588
bip-zip <bipindhakal44@gmail.com>
2023-03-19 21:31:13 +0545

    add playlist models

--- musicmaker/models.py
+++ musicmaker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.template.defaultfilters import date
 
-# Create your models here.
+class Category(models.Model):
+    pass
+
+class Playlist(models.Model):
+    pass
