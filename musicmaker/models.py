a7c6017c9c3224a1fcd17a72977f8e12cf89f9b1
bip-zip <bipindhakal44@gmail.com>
2023-03-20 19:52:32 +0545

    add playlist model

--- musicmaker/models.py
+++ musicmaker/models.py
@@ -3,8 +3,17 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.template.defaultfilters import date
 
-class Category(models.Model):
-    pass
 
 class Playlist(models.Model):
-    pass
+    name  = models.CharField(max_length=100, null=False)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='maker')
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    link = models.URLField(null=False)
--- musicmaker/views.py
+++ musicmaker/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render,redirect
+from django.views.generic import TemplateView, ListView
+from .models import Playlist
 
 class IndexView(TemplateView):
     template_name = 'musicmaker/index.html'
+
+    def post(self, request, *args, **kwargs):
+        playlistname = request.POST['name']
+        obj = Playlist.objects.create(user=request.user, name=playlistname)
+        obj.save()
+        return redirect('/')
+    
+
+class UserPlaylistView(ListView):
+    model = Playlist
+    template_name = 'musicmaker/userlist.html'
+
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
 
